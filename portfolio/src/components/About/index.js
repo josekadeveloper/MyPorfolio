a778432059632ec7c0f6b6fb97c662920fc39d68
jockdeveloper <jockdeveloper@gmail.com>
2022-08-27 22:23:02 +0200

    Creando Page Loader

--- portfolio/src/components/About/index.js
+++ portfolio/src/components/About/index.js
@@ -3,6 +3,7 @@ import AnimatedLetters from '../AnimatedLetters'
 import { useEffect, useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCss3, faHtml5, faJava, faJsSquare, faPhp, faReact } from '@fortawesome/free-brands-svg-icons'
+import Loader from 'react-loaders'
 
 const About = () => {
     const [letterClass, setLetterClass] = useState('text-animate')
@@ -14,58 +15,60 @@ const About = () => {
     }, [])
 
     return (
-        <div className="container about-page">
-            <div className="text-zone2">
-                <h1>
-                    <AnimatedLetters
-                        letterClass={letterClass}
-                        strArray={['A', 'b', 'o', 'u', 't', ' ', 'm', 'e']}
-                        idx={15}
-                    />                    
-                </h1>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-                    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-                    when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
-                    It has survived not only five centuries, but also the leap into electronic typesetting,
-                    remaining essentially unchanged.
-                </p>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-                    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-                    when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
-                    It has survived not only five centuries, but also the leap into electronic typesetting,
-                    remaining essentially unchanged.
-                </p>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-                    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-                    when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
-                    It has survived not only five centuries, but also the leap into electronic typesetting,
-                    remaining essentially unchanged.
-                </p>
-            </div>
-            <div className="stage-cube-cont">
-                <div className="cubespinner">
-                    <div className="face1">
-                        <FontAwesomeIcon icon={faPhp} />
-                    </div>
-                    <div className="face2">
-                        <FontAwesomeIcon icon={faHtml5} />
-                    </div>
-                    <div className="face3">
-                        <FontAwesomeIcon icon={faCss3} />
-                    </div>
-                    <div className="face4">
-                        <FontAwesomeIcon icon={faReact} />
-                    </div>
-                    <div className="face5">
-                        <FontAwesomeIcon icon={faJsSquare} />
-                    </div>
-                    <div className="face6">
-                        <FontAwesomeIcon icon={faJava} />
+        <>
+            <div className="container about-page">
+                <div className="text-zone2">
+                    <h1>
+                        <AnimatedLetters
+                            letterClass={letterClass}
+                            strArray={['A', 'b', 'o', 'u', 't', ' ', 'm', 'e']}
+                            idx={15}
+                        />                    
+                    </h1>
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+                        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+                        when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+                        It has survived not only five centuries, but also the leap into electronic typesetting,
+                        remaining essentially unchanged.
+                    </p>
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+                        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+                        when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+                        It has survived not only five centuries, but also the leap into electronic typesetting,
+                        remaining essentially unchanged.
+                    </p>
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+                        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+                        when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+                        It has survived not only five centuries, but also the leap into electronic typesetting,
+                        remaining essentially unchanged.
+                    </p>
+                </div>
+                <div className="stage-cube-cont">
+                    <div className="cubespinner">
+                        <div className="face1">
+                            <FontAwesomeIcon icon={faPhp} />
+                        </div>
+                        <div className="face2">
+                            <FontAwesomeIcon icon={faHtml5} />
+                        </div>
+                        <div className="face3">
+                            <FontAwesomeIcon icon={faCss3} />
+                        </div>
+                        <div className="face4">
+                            <FontAwesomeIcon icon={faReact} />
+                        </div>
+                        <div className="face5">
+                            <FontAwesomeIcon icon={faJsSquare} />
+                        </div>
+                        <div className="face6">
+                            <FontAwesomeIcon icon={faJava} />
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-
+            <Loader type="line-spin-fade-loader"/>
+        </>
     )
 }
 
--- portfolio/src/components/Home/index.js
+++ portfolio/src/components/Home/index.js
@@ -4,6 +4,7 @@ import './index.scss';
 import { useEffect, useState } from 'react';
 import AnimatedLetters from '../AnimatedLetters';
 import Logo from './Logo';
+import Loader from 'react-loaders'
 
 const Home = () => {
     const [letterClass, setLetterClass] = useState('text-animate')
@@ -17,28 +18,31 @@ const Home = () => {
     }, [])
 
     return (
-        <div className="container home-page">
-            <div className="text-zone">
-                <h1>
-                    <span className={letterClass}>H</span>
-                    <span className={`${letterClass} _12`}>i,</span>
-                    <br />
-                    <span className={`${letterClass} _13`}>I</span>
-                    <span className={`${letterClass} _14`}>'m</span>
-                    <img src={LogoTitle} alt="developer" />
+        <>
+            <div className="container home-page">
+                <div className="text-zone">
+                    <h1>
+                        <span className={letterClass}>H</span>
+                        <span className={`${letterClass} _12`}>i,</span>
+                        <br />
+                        <span className={`${letterClass} _13`}>I</span>
+                        <span className={`${letterClass} _14`}>'m</span>
+                        <img src={LogoTitle} alt="developer" />
+                            <AnimatedLetters letterClass={letterClass}
+                            strArray={nameArray}
+                            idx={15} />
+                        < br/>
                         <AnimatedLetters letterClass={letterClass}
-                        strArray={nameArray}
-                        idx={15} />
-                    < br/>
-                    <AnimatedLetters letterClass={letterClass}
-                        strArray={jobArray}
-                        idx={19} />
-                </h1>
-                <h2>FullStack Developer</h2>
-                <Link to="/contact" className="flat-button">CONTACT ME</Link>
+                            strArray={jobArray}
+                            idx={19} />
+                    </h1>
+                    <h2>FullStack Developer</h2>
+                    <Link to="/contact" className="flat-button">CONTACT ME</Link>
+                </div>
+                <Logo />
             </div>
-            <Logo />
-        </div>
+            <Loader type="line-spin-fade-loader" />
+        </>
     )
 }
 
