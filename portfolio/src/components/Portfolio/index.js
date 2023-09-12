f7b9dcf8c67a3c8acc1661f6ab4357b596f0da3c
josekadeveloper <josekadeveloper@gmail.com>
2023-09-12 10:41:25 +0200

    correct responsive design

--- portfolio/src/components/Portfolio/index.js
+++ portfolio/src/components/Portfolio/index.js
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from "react";
 import Loader from "react-loaders";
 import AnimatedLetters from "../AnimatedLetters";
-import "./index.scss";
 import { getDocs, collection } from "firebase/firestore";
 import { db } from "../../firebase";
 import { useTranslation } from "react-i18next";
+import "./index.scss";
 
 const Portfolio = () => {
     const [letterClass, setLetterClass] = useState('text-animate');
