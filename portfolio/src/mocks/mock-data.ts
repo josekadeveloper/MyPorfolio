1e1ae8358c021bc29c3ad49d707528cad6e6dc3d
josekadeveloper <josekadeveloper@gmail.com>
2025-04-28 21:41:40 +0200

    feature: add retrobank project

--- portfolio/src/mocks/mock-data.ts
+++ portfolio/src/mocks/mock-data.ts
@@ -1,6 +1,7 @@
 import {
   CALCULATOR_IMAGE,
   QUIZ_IMAGE,
+  RETROBANK_IMAGE,
   SPORTS_IMAGE,
   TICTACTOE_IMAGE,
   WEATHER_IMAGE,
@@ -9,34 +10,41 @@ import {
 export const mockData = [
   {
     id: '1',
+    name: 'RetroBank',
+    description: 'React, Typescript, Sass, Vite',
+    image: RETROBANK_IMAGE,
+    url: 'https://retro-bank-frontend.vercel.app/',
+  },
+  {
+    id: '2',
     name: 'QUIZ',
     description: 'React, Typescript, Sass',
     image: QUIZ_IMAGE,
     url: 'https://quizzz-game-app.netlify.app/',
   },
   {
-    id: '2',
+    id: '3',
     name: 'Sport Breaking News',
     description: 'React, JavaScript, Tailwind, Sass',
     image: SPORTS_IMAGE,
     url: 'https://sport-breaking-news.netlify.app/',
   },
   {
-    id: '3',
+    id: '4',
     name: 'Weather App',
     description: 'React, JavaScript, CSS',
     image: WEATHER_IMAGE,
     url: 'https://weather-app-dde6e.web.app/',
   },
   {
-    id: '4',
+    id: '5',
     name: 'Tic Tac Toe',
     description: 'React, Typescript, Sass',
     image: TICTACTOE_IMAGE,
     url: 'https://tik-tak-to3.netlify.app/',
   },
   {
-    id: '5',
+    id: '6',
     name: 'Calculator',
     description: 'React, Typescript, Sass',
     image: CALCULATOR_IMAGE,
